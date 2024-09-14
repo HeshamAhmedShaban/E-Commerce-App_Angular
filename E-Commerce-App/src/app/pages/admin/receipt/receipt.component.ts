1f6dd9128e9e1c8b6106afdaf4264fd17c57864b
Hesham Ahmed Shaban <154767851+HeshamAhmedShaban@users.noreply.github.com>
2024-09-14 07:28:44 +0300

    Rxjs

--- E-Commerce-App/src/app/pages/admin/receipt/receipt.component.ts
+++ E-Commerce-App/src/app/pages/admin/receipt/receipt.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { combineLatest, concatMap, delay, forkJoin, from, mergeMap, of, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-receipt',
@@ -7,6 +8,32 @@ import { Component } from '@angular/core';
   templateUrl: './receipt.component.html',
   styleUrl: './receipt.component.css'
 })
-export class ReceiptComponent {
+export class ReceiptComponent implements OnInit {
+
+
+  public switchMap$ = from([1,2,3,4,5]).pipe(
+    concatMap(value => {
+      console.log('Switching to Observable', value);
+      return of(`Observable value: ${value}`);
+    })
+  )
+
+  public forkJoin$ = forkJoin([this.switchMap$,of([1,2])]).pipe(
+    delay(500)
+  )
+
+
+  ngOnInit(): void {
+
+    // this.switchMap$.subscribe(data => {
+    //   console.log('Received data', data);
+    // });
+
+
+    // this.forkJoin$.subscribe(data => {
+    //   console.log('Received forkJoin data', data);
+    // });
+  
+  }
 
 }
